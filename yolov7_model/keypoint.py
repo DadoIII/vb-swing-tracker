6c08b8e1ccffd991d9467017075a7a307738acdd
DadoIII <daniel.zelinka9@gmail.com>
2024-02-29 14:41:55 +0000

    Processing video in batches works

--- yolov7_model/keypoint.py
+++ yolov7_model/keypoint.py
@@ -22,85 +22,95 @@ _ = model.float().eval()
 if torch.cuda.is_available():
     model.half().to(device)
 
-def locate_keypoints(image):
-    image = letterbox(image, 960, stride=64, auto=True)[0]
-    #image_ = image.copy()
-    image = transforms.ToTensor()(image)
-    image = torch.tensor(np.array([image.numpy()]))
+def locate_keypoints_batch(images):
+    in_images = torch.tensor([]).half().to(device)
+    for image in images:
+        in_image, ratio, padding = letterbox(image.copy(), 960, stride=64, auto=True)
+        in_image = transforms.ToTensor()(in_image)
+        in_image = torch.tensor(np.array([in_image.numpy()]))
+        if torch.cuda.is_available():
+            in_image = in_image.half().to(device)
+        in_images = torch.cat([in_images, torch.tensor(in_image)])
 
-    if torch.cuda.is_available():
-        image = image.half().to(device)   
-    output, _ = model(image)
+    outputs, _ = model(in_images)
         
     with torch.no_grad():
-        output = non_max_suppression_kpt(output, 0.25, 0.65, nc=model.yaml['nc'], nkpt=model.yaml['nkpt'], kpt_label=True)
-        output = output_to_keypoint(output)
-    nimg = image[0].permute(1, 2, 0) * 255
-    nimg = nimg.cpu().numpy().astype(np.uint8)
-    nimg = cv2.cvtColor(nimg, cv2.COLOR_RGB2BGR)
-    for idx in range(output.shape[0]):
-        plot_skeleton_kpts(nimg, output[idx, 7:].T, 3)
+        outputs = non_max_suppression_kpt(outputs, 0.25, 0.65, nc=model.yaml['nc'], nkpt=model.yaml['nkpt'], kpt_label=True)
+        outputs = torch.from_numpy(output_to_keypoint(outputs))
+    
+    # Rescale output to original image
+    paddings = torch.tensor([padding[0], padding[1], 0])
+    factors = torch.tensor([1 / ratio[0], 1 / ratio[1], 1])
+    repeat_factors = factors.tile((outputs.shape[1] - 7) // 3)
+    repeat_paddings = paddings.tile((outputs.shape[1] - 7) // 3)
+    outputs = torch.cat([outputs[:, :7], (outputs[:, 7:] - repeat_paddings) * repeat_factors], dim=1)
 
-    return nimg
+    plotted_images = []
+    for i, image in enumerate(images):
+        # Prepare image to plot the keypoints and work with openCV
+        image = transforms.ToTensor()(image)
+        image = torch.tensor(np.array([image.numpy()]))
+        image = image[0].permute(1, 2, 0) * 255
+        image = image.cpu().numpy().astype(np.uint8)
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
+        tensor_batch = outputs[outputs[:, 0] == i, :]
+        for idx in range(tensor_batch.shape[0]):
+            plot_skeleton_kpts(image, tensor_batch[idx, 7:].T, 3)
 
-# for i in range(100):
-#     print(f"Total gpu memory allocated: {torch.cuda.memory_allocated()}")
-#     image = cv2.imread(f'./test_images/test{1}.jpg')
-#     locate_keypoints(image)
+        plotted_images.append(image)
 
+    return plotted_images
 
-# for i in range(5):
-#     image = cv2.imread(f'./test_images/test{i+1}.jpg')
-
-#     nimg = locate_keypoints(image)
-
-#     plt.figure(figsize=(8,8))
-#     plt.axis('off')
-#     plt.imshow(nimg)
-
-#     # Save the plot to a file
-#     plt.savefig(f'output_image{i+1}.png')
-
-
-video_path = './test_images/video1.MOV'
+video_path = './test_images/Joh2.mp4'
 output_path = './test_images/video1_with_keypoints.avi'
-
-monitor_width, monitor_height = 600, 1000  # Change this to your monitor's resolution
+batch_size = 6
 
 cap = cv2.VideoCapture(video_path)
 
 # Get input video properties
 fps = int(cap.get(cv2.CAP_PROP_FPS))
+frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+print("fps", fps)
+print("Frame width:", frame_width)
+print("Frame height:", frame_height)
 
 # Create VideoWriter object to save the output video
 fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-#fourcc = cv2.VideoWriter_fourcc(*'XVID')
-#fourcc = cv2.VideoWriter_fourcc(*'H264')
-out = cv2.VideoWriter(output_path, fourcc, fps, (monitor_width, monitor_height))
+out = cv2.VideoWriter(output_path, fourcc, fps, (frame_width, frame_height))
+
+#import time
+#start_time = time.time()
 
 while cap.isOpened():
-    ret, frame = cap.read()
-    # if frame is read correctly ret is True
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
+    # Read a batch of frames
+    frames = []
+    for _ in range(batch_size):
+        ret, frame = cap.read()
+        if not ret:
+            break
+        frames.append(frame)
+
+    if len(frames) == 0:
         break
 
-    # Call locate_keypoints function
-    frame_with_keypoints = locate_keypoints(frame)
+    # Call locate_keypoints function to process the batch of frames
+    frames_with_keypoints = locate_keypoints_batch(frames)
 
-    frame_with_keypoints = cv2.cvtColor(frame_with_keypoints, cv2.COLOR_RGB2BGR)
+    # Convert frames with keypoints back to list of frames
+    frames_with_keypoints = [cv2.cvtColor(frame, cv2.COLOR_RGB2BGR) for frame in frames_with_keypoints]
 
-    # Resize the frame to fit the monitor's resolution
-    frame_with_keypoints = cv2.resize(frame_with_keypoints, (monitor_width, monitor_height))
-
-    # Write frame with keypoints to output video multiple times
-    out.write(frame_with_keypoints)
-
-    cv2.imshow('frame', frame_with_keypoints)
+    # Write the frames with keypoints to the output video
+    for frame_keypoints in frames_with_keypoints:
+        out.write(frame_keypoints)
 
     if cv2.waitKey(1) == ord('q'):
         break
+
+# Time the inference time
+#print("Elapsed time:", time.time() - start_time, "seconds")
 
 cap.release()
 out.release()
